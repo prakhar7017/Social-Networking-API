2374ba55e512283173280ca3016e5d1496f43c8f
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-03-16 19:11:53 +0530

    v3

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,9 @@ class Express{
         this.app.use(userRoutes);
         this.app.use(postRoutes);
         this.app.use(followRoute);
+        this.app.use('/health', (req, res) => {
+            res.status(200).send('Server is running');
+        });
 
         this.app.use((req, res, next) => {
             next(createError(404));
@@ -33,9 +36,7 @@ class Express{
     }
 
     public listen(): void{
-        this.app.get('/health', (req, res) => {
-            res.status(200).send('Server is running');
-        });
+       
         this.server.listen(config.server.port, () => {
             console.log(`Server running on port ${config.server.port}`);
         });
